250adba380a42e498c4cb646e96012a2283283e7
Robert <najman.robert@wp.pl>
2022-11-19 23:50:16 +0100

    Little updates for next ideas

--- src/legality_determination/KingLegality.cpp
+++
@@ -1,6 +0,0 @@
-#include "../../headers/legality_determination/KingLegality.hpp"
-
-bool KingLegality::legalityChecker(std::vector < std::vector <Field>> &board, int x, int y)
-{
-    //checkboard oh yeah
-}
